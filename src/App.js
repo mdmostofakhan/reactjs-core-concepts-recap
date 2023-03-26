76c2dc048525be44a6a980eca369557ce0bf953d
mostofa khan <mdmostofakhan1972@gmail.com>
2023-03-26 20:28:47 +0600

    recapss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPost></LoadPost>
+      <District name='Mymonsing' special='Netrokuna'></District>
+      <District name='Borishal' special='SodorGat'></District>
+      <District name='Noakhali' special='bolg'></District>
     </div>
   );
+}
+
+//second functon componets-(1)-
+function LoadPost () {
+ const [posts, setPosts] = useState([]);
+ 
+ useEffect( () =>{
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(data => setPosts(data))
+ }, [])
+
+ return(
+  <div>
+    <h1>Posts:{posts.length}</h1>
+    {
+      posts.map(post => <Post title={post.title} body={post.body}
+        userId={post.userId}
+        ></Post>)
+    }
+  </div>
+ )
+}
+// third componets-(1)--
+function Post(props){
+  return(
+    <div style={{backgroundColor: 'lightgray', padding: '15px', margin: '20px', border: '4px solid red', borderRadius: '20px'}}>
+      <h1>Title:{props.title}</h1>
+      <p>Body:{props.body}</p>
+      <p>UserId:{props.userId}</p>
+    </div>
+  )
+}
+
+// style direectiion---
+const districtStyle = {
+  backgroundColor:'yellow',
+  margin:'10px',
+  borderRadius:'20px',
+  padding:'20px'
+
+}
+
+// function direction--- componet!
+function District(props){
+const [power, setPower] = useState(1);
+
+const boostPower = ()=>{
+const newPower = power * 2;
+setPower(newPower);
+}
+
+  return(
+    <div style={districtStyle}>
+      <h1>Name:{props.name}</h1>
+      <p>Specialty:{props.special}</p>
+      <h4>Power:{power}</h4>
+      <button onClick={boostPower}>Boost Power The</button>
+    </div>
+  )
 }
 
 export default App;
